2ca6817f845f1b24e050f8354939b8a75dc75171
xiaodi <758861884@qq.com>
2019-04-29 22:04:30 +0800

    update

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -144,13 +144,13 @@ export default {
     }
   },
   created () {
-    get2step({ })
-      .then(res => {
-        this.requiredTwoStepCaptcha = res.result.stepCode
-      })
-      .catch(() => {
-        this.requiredTwoStepCaptcha = false
-      })
+    // get2step({ })
+    //   .then(res => {
+    //     this.requiredTwoStepCaptcha = res.result.stepCode
+    //   })
+    //   .catch(() => {
+    //     this.requiredTwoStepCaptcha = false
+    //   })
     // this.requiredTwoStepCaptcha = true
   },
   methods: {
