bfe951ca02059d67ab81310c41fddee896b4932b
xiaodi <liangjinbiao@live.com>
2019-12-24 10:58:10 +0800

    修复 头像显示问题

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -29,7 +29,7 @@ const user = {
       if (avatar === null) {
         avatar = 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png'
       }
-      avatar = process.env.VUE_APP_API_BASE_URL + '/' + avatar
+      avatar = process.env.VUE_APP_API_BASE_HOST + '/' + avatar
 
       // 兼容coding.net上的后端（可删除此行代码）
       avatar = avatar.replace('/index.php', '')
