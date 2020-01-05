bb16462a59b439848257be7f3c126b80159c133c
xiaodi <liangjinbiao@live.com>
2020-01-05 15:23:02 +0800

    调整

--- src/utils/util.js
+++ src/utils/util.js
@@ -5,9 +5,7 @@ export function timeFix () {
 }
 
 export function welcome () {
-  const arr = ['休息一会儿吧', '准备吃什么呢?', '要不要打一把 DOTA', '我猜你可能累了']
-  const index = Math.floor(Math.random() * arr.length)
-  return arr[index]
+  return '祝你开心每一天！'
 }
 
 /**
