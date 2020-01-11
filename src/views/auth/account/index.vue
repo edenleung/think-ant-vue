a851745f8f7c9c5ba6497e2ec7bf2dab3099f89f
xiaodi <liangjinbiao@live.com>
2020-01-11 21:25:33 +0800

    添加 公司架构树

--- src/views/auth/account/index.vue
+++ src/views/auth/account/index.vue
@@ -92,108 +92,125 @@
       </a-form>
     </a-modal>
 
-    <a-card :body-style="{ padding: 0 }">
-      <!-- <div class="table-page-search-wrapper">
-        <a-form layout="inline">
-          <a-row :gutter="48">
-            <a-col :md="8" :sm="24">
-              <a-form-item label="角色ID">
-                <a-input placeholder="请输入"/>
-              </a-form-item>
-            </a-col>
-            <a-col :md="8" :sm="24">
-              <a-form-item label="状态">
-                <a-select placeholder="请选择" default-value="0">
-                  <a-select-option value="0">全部</a-select-option>
-                  <a-select-option value="1">关闭</a-select-option>
-                  <a-select-option value="2">运行中</a-select-option>
-                </a-select>
-              </a-form-item>
-            </a-col>
-            <a-col :md="8" :sm="24">
-              <span class="table-page-search-submitButtons">
-                <a-button type="primary">查询</a-button>
-                <a-button style="margin-left: 8px">重置</a-button>
-              </span>
-            </a-col>
-          </a-row>
-        </a-form>
-      </div> -->
-
-      <div class="ant-pro-table-toolbar">
-        <div class="ant-pro-table-toolbar-title">用户列表</div>
-        <div class="ant-pro-table-toolbar-option">
-          <div class="ant-pro-table-toolbar-item">
-            <a-button v-action:account-add type="primary" icon="plus" :loading="loading" @click="visible = true">新建</a-button>
-          </div>
-          <template v-if="selectedRows.length">
-            <div class="ant-pro-table-toolbar-item">
-              <a-dropdown>
-                <a-menu slot="overlay" @click="handleMenuClick">
-                  <a-menu-item key="remove"><a-icon type="delete" />批量删除</a-menu-item>
-                </a-menu>
-                <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /> </a-button>
-              </a-dropdown>
-            </div>
-          </template>
-          <a-divider type="vertical" />
-          <span class="ant-pro-table-toolbar-item-icon">
-            <a-tooltip placement="top">
-              <template slot="title">
-                <span>刷新</span>
-              </template>
-              <a-icon type="reload" @click="refreshTable()"/>
-            </a-tooltip>
-          </span>
-        </div>
-      </div>
-
-      <s-table
-        ref="table"
-        size="default"
-        :rowKey="(record) => record.id"
-        :columns="columns"
-        :data="loadData"
-        :alert="true"
-        :expandRowByClick="true"
-        :expandIcon="expandIcon"
-        :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
-      >
-
-        <template slot="dept" slot-scope="row">
-          <a-tag color="purple">{{ row.dept_name }}</a-tag>
-        </template>
-
-        <template slot="status" slot-scope="row">
-          <template v-if="row.status === 1">正常</template>
-          <template v-else>禁用</template>
-        </template>
-
-        <template slot="expandedRowRender" slot-scope="row">
-          <a-row>
-            <a-col class="rule-list" span="12" v-for="(item, index) in rules" :key="index">
-              <a-row>
-                <a-col span="4">{{ item.title }}：</a-col>
-                <a-col span="20">
-                  <template v-if="!item.actions.length">-</template>
-                  <template v-for="(action, i) in item.actions">
-                    <a-tag :key="i" v-if="row.rules.indexOf(action.id) !== -1">
-                      {{ action.title }}
-                    </a-tag>
-                  </template>
+    <a-row :gutter="16">
+      <a-col :xs="{span: 24}" :lg="4">
+        <a-card :body-style="{ padding: '0 24px' }">
+          <a-tree
+            @expand="onExpand"
+            :expandedKeys="expandedKeys"
+            :autoExpandParent="autoExpandParent"
+            v-model="checkedKeys"
+            @select="onSelects"
+            :selectedKeys="selectedKeys"
+            :treeData="depts"
+          />
+        </a-card>
+      </a-col>
+      <a-col :xs="{span: 24}" :lg="20">
+        <a-card :body-style="{ padding: 0 }">
+          <!-- <div class="table-page-search-wrapper">
+            <a-form layout="inline">
+              <a-row :gutter="48">
+                <a-col :md="8" :sm="24">
+                  <a-form-item label="角色ID">
+                    <a-input placeholder="请输入"/>
+                  </a-form-item>
+                </a-col>
+                <a-col :md="8" :sm="24">
+                  <a-form-item label="状态">
+                    <a-select placeholder="请选择" default-value="0">
+                      <a-select-option value="0">全部</a-select-option>
+                      <a-select-option value="1">关闭</a-select-option>
+                      <a-select-option value="2">运行中</a-select-option>
+                    </a-select>
+                  </a-form-item>
+                </a-col>
+                <a-col :md="8" :sm="24">
+                  <span class="table-page-search-submitButtons">
+                    <a-button type="primary">查询</a-button>
+                    <a-button style="margin-left: 8px">重置</a-button>
+                  </span>
                 </a-col>
               </a-row>
-            </a-col>
-          </a-row>
-        </template>
+            </a-form>
+          </div> -->
 
-        <template slot="tools" slot-scope="row">
-          <a v-action:account-update @click="openInfoModal(row)">编辑</a>
-          <a-divider type="vertical" />
-          <a v-action:account-delete @click="showDeleteConfirm(row.id)">删除</a>
-        </template>
-      </s-table>
-    </a-card>
+          <div class="ant-pro-table-toolbar">
+            <div class="ant-pro-table-toolbar-title"></div>
+            <div class="ant-pro-table-toolbar-option">
+              <div class="ant-pro-table-toolbar-item">
+                <a-button type="primary" icon="plus" :loading="loading" @click="visible = true">新建</a-button>
+              </div>
+              <template v-if="selectedRows.length">
+                <div class="ant-pro-table-toolbar-item">
+                  <a-dropdown>
+                    <a-menu slot="overlay" @click="handleMenuClick">
+                      <a-menu-item key="remove"><a-icon type="delete" />批量删除</a-menu-item>
+                    </a-menu>
+                    <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /> </a-button>
+                  </a-dropdown>
+                </div>
+              </template>
+              <a-divider type="vertical" />
+              <span class="ant-pro-table-toolbar-item-icon">
+                <a-tooltip placement="top">
+                  <template slot="title">
+                    <span>刷新</span>
+                  </template>
+                  <a-icon type="reload" @click="refreshTable()"/>
+                </a-tooltip>
+              </span>
+            </div>
+          </div>
+
+          <s-table
+            ref="table"
+            size="default"
+            :rowKey="(record) => record.id"
+            :columns="columns"
+            :data="loadData"
+            :alert="true"
+            :expandRowByClick="true"
+            :expandIcon="expandIcon"
+            :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
+          >
+
+            <template slot="dept" slot-scope="row">
+              <a-tag color="purple">{{ row.dept_name }}</a-tag>
+            </template>
+
+            <template slot="status" slot-scope="row">
+              <template v-if="row.status === 1">正常</template>
+              <template v-else>禁用</template>
+            </template>
+
+            <template slot="expandedRowRender" slot-scope="row">
+              <a-row>
+                <a-col class="rule-list" span="12" v-for="(item, index) in rules" :key="index">
+                  <a-row>
+                    <a-col span="4">{{ item.title }}：</a-col>
+                    <a-col span="20">
+                      <template v-if="!item.actions.length">-</template>
+                      <template v-for="(action, i) in item.actions">
+                        <a-tag :key="i" v-if="row.rules.indexOf(action.id) !== -1">
+                          {{ action.title }}
+                        </a-tag>
+                      </template>
+                    </a-col>
+                  </a-row>
+                </a-col>
+              </a-row>
+            </template>
+
+            <template slot="tools" slot-scope="row">
+              <a v-action:account-update @click="openInfoModal(row)">编辑</a>
+              <a-divider type="vertical" />
+              <a v-action:account-delete @click="showDeleteConfirm(row.id)">删除</a>
+            </template>
+          </s-table>
+        </a-card>
+      </a-col>
+    </a-row>
   </div>
 </template>
 <script>
@@ -233,6 +250,10 @@ export default {
       rules: [],
       roles: [],
       depts: [],
+      expandedKeys: [],
+      autoExpandParent: true,
+      checkedKeys: [],
+      selectedKeys: [],
       form: this.$form.createForm(this),
       columns,
       selected: 0,
@@ -245,6 +266,8 @@ export default {
           this.roles = roles
           this.depts = depts
 
+          // 展开组织架构
+          this.expandedDept(depts)
           return users
         })
       },
@@ -336,7 +359,49 @@ export default {
         })
         return false
       }
+    },
+    onExpand (expandedKeys) {
+      console.log('onExpand', expandedKeys)
+      // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+      // or, you can remove all expanded children keys.
+      this.expandedKeys = expandedKeys
+      this.autoExpandParent = false
+    },
+    onCheck (checkedKeys) {
+      console.log('onCheck', checkedKeys)
+      this.checkedKeys = checkedKeys
+    },
+    onSelects (selectedKeys, info) {
+      console.log('onSelect', info)
+      this.$message.info('未开发')
+      this.selectedKeys = selectedKeys
+    },
+    expandedDept (depts) {
+      depts.map(dept => {
+        this.expandedKeys.push(dept.value)
+        if (dept.children !== undefined) {
+          this.expandedDept(dept.children)
+        }
+      })
     }
   }
 }
 </script>
+
+<style lang="scss">
+.company {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 64px;
+    padding: 0 24px;
+    line-height: 64px;
+    &-title {
+      flex: 1 1;
+      color: #000;
+      font-size: 16px;
+      line-height: 24px;
+      opacity: .85;
+    }
+}
+</style>
