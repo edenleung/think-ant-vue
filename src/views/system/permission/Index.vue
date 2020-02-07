2c5d8499cb47e82c16b0da5b9c5a3d76c13f03dc
xiaodi <liangjinbiao@live.com>
2020-02-07 13:05:20 +0800

    调整

--- src/views/system/permission/Index.vue
+++ src/views/system/permission/Index.vue
@@ -1,223 +1,5 @@
 <template>
   <div class="page-header-index-wide">
-    <a-modal
-      :title="selected ? '修改菜单' : '添加菜单'"
-      :visible="visible"
-      :width="650"
-      @ok="handleSubmit"
-      :confirmLoading="confirmLoading"
-      @cancel="handleCancel">
-      <a-form :form="form" layout="inline">
-        <a-row :gutter="0">
-          <a-col :span="12">
-            <a-form-item label="菜单类型">
-              <a-radio-group
-                v-decorator="[
-                  'type',
-                  {
-                    rules: [{ required: true, message: '请选择类型!' }]
-                  }
-                ]"
-              >
-                <a-radio-button :value="type.value" v-for="type in typeMap" :key="type.value">{{ type.text }}</a-radio-button>
-              </a-radio-group>
-            </a-form-item>
-          </a-col>
-
-          <a-col :span="12">
-            <a-form-item label="上级类目" hasFeedback>
-              <a-tree-select
-                style="min-width:171px"
-                :dropdownStyle="{ maxHeight: '400px', overflow: 'auto' }"
-                placeholder="请选择所属组别"
-                allowClear
-                treeDefaultExpandAll
-                :treeData="tree"
-                @change="handleTreeChange"
-                v-decorator="[
-                  'pid',
-                  {
-                    rules: [{ required: true, message: '请选择所属组别!' }]
-                  }
-                ]"
-              >
-              </a-tree-select>
-            </a-form-item>
-          </a-col>
-
-          <a-col :span="12">
-            <a-form-item label="菜单标题" hasFeedback>
-              <a-input
-                placeholder="菜单标题"
-                :readonly="form.getFieldValue('type') === 'action'"
-                v-decorator="[
-                  'title',
-                  {
-                    rules: [{ required: true, message: '请输入菜单标题!' }]
-                  }
-                ]"
-              />
-            </a-form-item>
-          </a-col>
-
-          <a-col :span="12">
-            <a-form-item label="权限标识" hasFeedback>
-              <a-input
-                placeholder="唯一识别码"
-                :disabled="this.selected !== 0"
-                v-decorator="[
-                  'name',
-                  {
-                    rules: [{ required: true, message: '请输入唯一识别码!' }]
-                  }
-                ]"
-              />
-            </a-form-item>
-          </a-col>
-
-          <a-col :span="24">
-            <a-col :span="12">
-              <a-form-item v-show="form.getFieldValue('type') !== 'action'" label="组件地址">
-                <a-select
-                  style="width: 171px"
-                  v-decorator="[
-                    'component',
-                    {
-                      rules: [{ required: form.getFieldValue('type') !== 'action', message: '请选择组件!' }]
-                    }
-                  ]">
-                  >
-                  <a-select-opt-group v-for="(list, type) in Components" :key="type" :label="type">
-                    <a-select-option :value="name" v-for="(item, name) in list" :key="name">{{ name }}</a-select-option>
-                  </a-select-opt-group>
-                </a-select>
-              </a-form-item>
-            </a-col>
-
-            <a-col :span="12" v-show="form.getFieldValue('type') !== 'action'">
-              <a-form-item label="路由地址" hasFeedback>
-                <a-input
-                  placeholder="路由地址"
-                  v-decorator="[
-                    'path',
-                    {
-                      rules: [{ required: form.getFieldValue('type') !== 'action', message: '请填写路由地址!' }]
-                    }
-                  ]"
-                />
-              </a-form-item>
-            </a-col>
-          </a-col>
-          <a-col :span="24">
-            <a-form-item v-show="form.getFieldValue('type') === 'path'" label="Redirect">
-              <a-input
-                v-decorator="[
-                  'redirect',
-                  {
-                    rules: [{ required: form.getFieldValue('type') === 'path', message: '请重定向地址!' }]
-                  }
-                ]"
-              >
-              </a-input>
-            </a-form-item>
-          </a-col>
-
-          <a-col :span="24">
-            <a-col :span="12">
-              <a-form-item label="菜单状态">
-                <a-radio-group
-                  v-decorator="[
-                    'status',
-                    {
-                      rules: [{ required: true, message: '请选择菜单状态!' }],
-                      initialValue: 1
-                    }
-                  ]"
-                >
-                  <a-radio-button :key="key" :value="type.value" v-for="(type, key) in status">{{ type.label }}</a-radio-button>
-                </a-radio-group>
-              </a-form-item>
-            </a-col>
-
-            <a-col :span="12">
-              <a-form-item label="菜单缓存" v-show="form.getFieldValue('type') !== 'action'">
-                <a-radio-group
-                  v-decorator="[
-                    'keepAlive',
-                    {
-                      rules: [{ required: true, message: '请选择菜单缓存!' }],
-                      initialValue: 0
-                    }
-                  ]"
-                >
-                  <a-radio-button :key="key" :value="type.value" v-for="(type, key) in status">{{ type.label }}</a-radio-button>
-                </a-radio-group>
-              </a-form-item>
-            </a-col>
-
-            <a-col :span="12">
-              <a-form-item label="菜单可见" v-show="form.getFieldValue('type') !== 'action'">
-                <a-radio-group
-                  v-decorator="[
-                    'visable',
-                    {
-                      rules: [{ required: true, message: '请选择菜单可见!' }],
-                      initialValue: 1
-                    }
-                  ]"
-                >
-                  <a-radio-button :key="key" :value="type.value" v-for="(type, key) in status">
-                    {{ type.label }}</a-radio-button>
-                </a-radio-group>
-              </a-form-item>
-            </a-col>
-            <a-col :span="12">
-              <a-form-item label="隐藏子菜单" v-show="form.getFieldValue('type') !== 'action'">
-                <a-radio-group
-                  v-decorator="[
-                    'hideChildrenInMenu',
-                    {
-                      rules: [{ required: true, message: '请选择隐藏子菜单!' }],
-                      initialValue: 0
-                    }
-                  ]"
-                >
-                  <a-radio-button :key="key" :value="type.value" v-for="(type, key) in status">{{ type.label }}</a-radio-button>
-                </a-radio-group>
-              </a-form-item>
-            </a-col>
-          </a-col>
-
-          <a-col :span="12">
-            <a-form-item v-show="form.getFieldValue('type') !== 'action'" label="查看权限">
-              <a-select
-                style="min-width: 171px"
-                mode="tags"
-                placeholder="选择查看权限"
-                v-decorator="[
-                  'permission'
-                ]"
-              >
-              </a-select>
-            </a-form-item>
-          </a-col>
-
-          <a-col :span="12">
-            <a-form-item v-show="form.getFieldValue('type') !== 'action'" label="菜单图标">
-              <a-input
-                slot="default"
-                style="width: 171px;"
-                v-decorator="[
-                  'icon'
-                ]">
-                <a-icon slot="prefix" type="search" />
-              </a-input>
-            </a-form-item>
-          </a-col>
-        </a-row>
-      </a-form>
-    </a-modal>
-
     <a-card :body-style="{ padding: 0 }">
       <div class="ant-pro-table-toolbar">
         <div class="ant-pro-table-toolbar-title">规则列表</div>
@@ -269,7 +51,7 @@
                   v-action:PermissionUpdate
                   type="primary"
                   ghost
-                  @click="openActionModal(action)"
+                  @click="showActionModal(action)"
                   style="margin-right:10px">编辑</a-button>
                 <a-button size="small" v-action:PermissionDelete type="danger" ghost @click="showDeleteConfirm(action.id)">删除</a-button>
               </template>
@@ -291,56 +73,31 @@
         </template>
 
         <template slot="tools" slot-scope="row">
-          <a v-action:PermissionUpdate @click="openActionModal(row)">编辑</a>
+          <a v-action:PermissionUpdate @click="showActionModal(row)">编辑</a>
           <a-divider type="vertical" />
           <a v-action:PermissionDelete @click="showDeleteConfirm(row.id)">删除</a>
         </template>
       </s-table>
     </a-card>
+
+    <PermissionForm
+      ref="permissionForm"
+      :visible="visible"
+      :selected="selected"
+      :status="status"
+      :typeMap="typeMap"
+      :treeData="roleTree"
+      :confirmLoading="confirmLoading"
+      @submit="handleSubmit"
+      @cancel="handleCancel"
+    />
   </div>
 </template>
 <script>
 import { STable } from '@/components'
-import { Components } from '@/config/componentConfigs'
+import PermissionForm from './components/PermissionForm'
 import { fetchPermission, addPermission, updatePermission, deletePermission } from '@/api/permission'
 const status = [{ label: '是', value: 1 }, { label: '否', value: 0 }]
-const dataSource = [
-  {
-    title: '话题',
-    children: [
-      {
-        title: 'AntDesign',
-        count: 10000
-      },
-      {
-        title: 'AntDesign UI',
-        count: 10600
-      }
-    ]
-  },
-  {
-    title: '问题',
-    children: [
-      {
-        title: 'AntDesign UI 有多好',
-        count: 60100
-      },
-      {
-        title: 'AntDesign 是啥',
-        count: 30010
-      }
-    ]
-  },
-  {
-    title: '文章',
-    children: [
-      {
-        title: 'AntDesign 是一个设计语言',
-        count: 100000
-      }
-    ]
-  }
-]
 const statusMap = {
   0: {
     status: 'default',
@@ -405,21 +162,14 @@ export default {
         '列表使用场景：后台管理中的权限管理以及角色管理，可用于基于 RBAC 设计的角色权限控制，颗粒度细到每一个操作类型。',
       columns,
       typeMap,
+      status,
       expandedKeys: [],
-      labelCol: {
-        xs: { span: 24 },
-        sm: { span: 5 }
-      },
-      wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 16 }
-      },
       queryParam: {},
       loadData: parameter => {
         return fetchPermission(Object.assign(parameter, this.queryParam)).then(res => {
           res = res.result
           const { tree, data } = res
-          this.tree = tree
+          this.roleTree = tree
           // 展开所有
           this.expandedRow(data)
           return res
@@ -428,26 +178,14 @@ export default {
       loading: false,
       visible: false,
       confirmLoading: false,
-      form: this.$form.createForm(this),
       info: {},
-      tree: [],
+      roleTree: [],
       selected: 0,
       selectedRowKeys: [],
-      selectedRows: [],
-      dataSource,
-      status,
-      Components
+      selectedRows: []
     }
   },
-  computed: {
-    actions () {
-      return this.$enum()
-    }
-  },
-  components: { STable },
-  mounted () {
-    console.log('$enum', this.$enum())
-  },
+  components: { STable, PermissionForm },
   filters: {
     statusFilter (type) {
       return statusMap[type].text
@@ -463,18 +201,19 @@ export default {
     }
   },
   methods: {
-    openActionModal (row) {
+    showActionModal (row) {
       this.visible = true
       this.selected = row.id
       this.$nextTick(() => {
-        this.form.setFieldsValue({
+        this.$refs.permissionForm.form.setFieldsValue({
           ...row,
+          pid: row.pid.toString(),
           permission: row.permission ? row.permission.split(',') : []
         })
       })
     },
     handleSubmit () {
-      this.form.validateFields((err, values) => {
+      this.$refs.permissionForm.form.validateFields((err, values) => {
         if (!err) {
           this.confirmLoading = true
           const promise = this.selected === 0 ? addPermission(values) : updatePermission(this.selected, values)
@@ -494,7 +233,7 @@ export default {
     handleCancel () {
       this.visible = false
       this.selected = 0
-      this.form.resetFields()
+      this.$refs.permissionForm.form.resetFields()
     },
     showDeleteConfirm (id) {
       this.$confirm({
@@ -557,10 +296,5 @@ export default {
   padding-right: 12px;
   margin-bottom: 12px;
   min-height: 23px;
-}
-.ant-form-inline .ant-form-item {
-    display: inline-block;
-    margin-right: 16px;
-    margin-bottom: 18px;
 }
 </style>
---
+++ src/views/system/permission/components/PermissionForm.vue
@@ -0,0 +1,273 @@
+<template>
+  <a-modal
+    :width="650"
+    :title="selected ? '修改菜单' : '添加菜单'"
+    :visible="visible"
+    :confirmLoading="confirmLoading"
+    @ok="$emit('submit')"
+    @cancel="$emit('cancel')">
+    <a-form :form="form" layout="inline">
+      <a-row :gutter="0">
+        <a-col :span="12">
+          <a-form-item label="菜单类型">
+            <a-radio-group
+              v-decorator="[
+                'type',
+                {
+                  rules: [{ required: true, message: '请选择类型!' }]
+                }
+              ]"
+            >
+              <a-radio-button :value="type.value" v-for="type in typeMap" :key="type.value">{{ type.text }}</a-radio-button>
+            </a-radio-group>
+          </a-form-item>
+        </a-col>
+
+        <a-col :span="12">
+          <a-form-item label="上级类目" hasFeedback>
+            <a-tree-select
+              style="min-width:171px"
+              :dropdownStyle="{ maxHeight: '400px', overflow: 'auto' }"
+              placeholder="请选择所属组别"
+              allowClear
+              treeDefaultExpandAll
+              :treeData="treeData"
+              @change="$emit('treeChange', $event)"
+              v-decorator="[
+                'pid',
+                {
+                  rules: [{ required: true, message: '请选择所属组别!' }]
+                }
+              ]"
+            >
+            </a-tree-select>
+          </a-form-item>
+        </a-col>
+
+        <a-col :span="12">
+          <a-form-item label="菜单标题" hasFeedback>
+            <a-input
+              placeholder="菜单标题"
+              v-decorator="[
+                'title',
+                {
+                  rules: [{ required: true, message: '请输入菜单标题!' }]
+                }
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+
+        <a-col :span="12">
+          <a-form-item label="权限标识" hasFeedback>
+            <a-input
+              placeholder="唯一识别码"
+              :disabled="this.selected !== 0"
+              v-decorator="[
+                'name',
+                {
+                  rules: [{ required: true, message: '请输入唯一识别码!' }]
+                }
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+
+        <a-col :span="24">
+          <a-col :span="12">
+            <a-form-item v-if="form.getFieldValue('type') !== 'action'" label="组件地址">
+              <a-select
+                style="width: 171px"
+                v-decorator="[
+                  'component',
+                  {
+                    rules: [{ required: form.getFieldValue('type') !== 'action', message: '请选择组件!' }]
+                  }
+                ]">
+                >
+                <a-select-opt-group v-for="(list, type) in Components" :key="type" :label="type">
+                  <a-select-option :value="name" v-for="(item, name) in list" :key="name">{{ name }}</a-select-option>
+                </a-select-opt-group>
+              </a-select>
+            </a-form-item>
+          </a-col>
+
+          <a-col :span="12" v-if="form.getFieldValue('type') !== 'action'">
+            <a-form-item label="路由地址" hasFeedback>
+              <a-input
+                placeholder="路由地址"
+                v-decorator="[
+                  'path',
+                  {
+                    rules: [{ required: form.getFieldValue('type') !== 'action', message: '请填写路由地址!' }]
+                  }
+                ]"
+              />
+            </a-form-item>
+          </a-col>
+        </a-col>
+        <a-col :span="24">
+          <a-form-item v-if="form.getFieldValue('type') === 'path'" label="Redirect">
+            <a-input
+              v-decorator="[
+                'redirect',
+                {
+                  rules: [{ required: form.getFieldValue('type') === 'path', message: '请重定向地址!' }]
+                }
+              ]"
+            >
+            </a-input>
+          </a-form-item>
+        </a-col>
+
+        <a-col :span="24">
+          <a-col :span="12">
+            <a-form-item label="菜单状态">
+              <a-radio-group
+                v-decorator="[
+                  'status',
+                  {
+                    rules: [{ required: true, message: '请选择菜单状态!' }],
+                    initialValue: 1
+                  }
+                ]"
+              >
+                <a-radio-button :key="key" :value="type.value" v-for="(type, key) in status">{{ type.label }}</a-radio-button>
+              </a-radio-group>
+            </a-form-item>
+          </a-col>
+
+          <a-col :span="12" v-if="form.getFieldValue('type') !== 'action'">
+            <a-form-item label="菜单缓存">
+              <a-radio-group
+                v-decorator="[
+                  'keepAlive',
+                  {
+                    rules: [{ required: true, message: '请选择菜单缓存!' }],
+                    initialValue: 0
+                  }
+                ]"
+              >
+                <a-radio-button :key="key" :value="type.value" v-for="(type, key) in status">{{ type.label }}</a-radio-button>
+              </a-radio-group>
+            </a-form-item>
+          </a-col>
+
+          <a-col :span="12" v-if="form.getFieldValue('type') !== 'action'">
+            <a-form-item label="菜单可见">
+              <a-radio-group
+                v-decorator="[
+                  'visable',
+                  {
+                    rules: [{ required: true, message: '请选择菜单可见!' }],
+                    initialValue: 1
+                  }
+                ]"
+              >
+                <a-radio-button :key="key" :value="type.value" v-for="(type, key) in status">
+                  {{ type.label }}</a-radio-button>
+              </a-radio-group>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" v-if="form.getFieldValue('type') !== 'action'">
+            <a-form-item label="隐藏子菜单">
+              <a-radio-group
+                v-decorator="[
+                  'hideChildrenInMenu',
+                  {
+                    rules: [{ required: true, message: '请选择隐藏子菜单!' }],
+                    initialValue: 0
+                  }
+                ]"
+              >
+                <a-radio-button :key="key" :value="type.value" v-for="(type, key) in status">{{ type.label }}</a-radio-button>
+              </a-radio-group>
+            </a-form-item>
+          </a-col>
+        </a-col>
+
+        <a-col :span="12" v-if="form.getFieldValue('type') !== 'action'" >
+          <a-form-item label="查看权限">
+            <a-select
+              style="min-width: 171px"
+              mode="tags"
+              placeholder="选择查看权限"
+              v-decorator="[
+                'permission'
+              ]"
+            >
+            </a-select>
+          </a-form-item>
+        </a-col>
+
+        <a-col :span="12" v-if="form.getFieldValue('type') !== 'action'" >
+          <a-form-item label="菜单图标">
+            <a-input
+              slot="default"
+              style="width: 171px;"
+              v-decorator="[
+                'icon'
+              ]">
+              <a-icon slot="prefix" type="search" />
+            </a-input>
+          </a-form-item>
+        </a-col>
+      </a-row>
+    </a-form>
+  </a-modal>
+</template>
+
+<script>
+import { Components } from '@/config/componentConfigs'
+
+export default {
+  props: {
+    selected: {
+      type: Number,
+      default: 0
+    },
+    treeData: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    status: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    typeMap: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    confirmLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      Components
+    }
+  },
+  beforeCreate () {
+    this.form = this.$form.createForm(this, { name: 'permission_from' })
+  }
+}
+</script>
+
+<style scoped>
+.ant-form-inline .ant-form-item {
+    display: inline-block;
+    margin-right: 16px;
+    margin-bottom: 18px;
+}
+</style>
